f81997e395e371214dd247de2496013f443f585e
lyfeyaj <lyfeyaj@gmail.com>
2015-08-01 00:14:36 +0800

    add autoload functionality

--- test/glue.test.js
+++ test/glue.test.js
@@ -5,6 +5,8 @@ var poplar = require('poplar');
 var ApiBuilder = poplar.ApiBuilder;
 var glue = require('../');
 
+glue.autoload('myLoaders', path.join(__dirname, './loaders'));
+
 var SampleApp = require('./sample_app');
 
 var expect = chai.expect;
@@ -13,6 +15,15 @@ describe('glue', function() {
   it('should have specific functions', function() {
     expect(glue).to.respondTo('init');
     expect(glue).to.respondTo('createFromApiBuilders');
+  });
+
+  it('should load autoload paths', function() {
+    expect(poplar.get('myLoaders')).to.have.property('middlewares');
+    expect(poplar.get('myLoaders')).to.have.property('config');
+    expect(poplar.get('myLoaders')).to.have.property('controllers');
+    expect(poplar.get('myLoaders')).to.have.property('entities');
+    expect(poplar.get('myLoaders')).to.have.property('environments');
+    expect(poplar.get('myLoaders')).to.have.property('initializers');
   });
 
   it('shoule be poplar instances', function() {
